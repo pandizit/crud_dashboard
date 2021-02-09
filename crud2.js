853df303508108419035b24d13293becd3e2fc06
pandizit <pandix@live.com>
2021-02-09 10:32:18 +0700

    update

--- crud2.js
+++ crud2.js
@@ -6,7 +6,7 @@ var session = require('express-session');
 const mongodb = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 8181;
 
 app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
 app.use('/jquery', express.static('node_modules/jquery/dist'));
